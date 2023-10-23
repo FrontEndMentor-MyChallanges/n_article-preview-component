dcee763b5c58fa466ece6d271777016e62b9ed8d
miroslaw-mroczkowski <mroczkowski.miroslaw@gmail.com>
2023-10-23 14:39:43 +0200

    Started mobile design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
 	content: ['./index.html', './src/*.{html,js}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				manrope: ['Manrope', ...defaultTheme.fontFamily.sans],
+			},
+			colors: {
+				customDarkGrayishBlue: '#48556a',
+				customDarkBlue: '#6d7f97',
+				customGrayishBlue: '#9eafc2',
+				customLightGrayishBlue: '#ecf2f8',
+			},
+		},
 	},
 	plugins: [],
 };
